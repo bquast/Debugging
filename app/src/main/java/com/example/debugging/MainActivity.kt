a738715816f6f4e94000937a6d6efaab15e3ffbe
Bastiaan Quast <bquast@gmail.com>
2021-12-26 15:50:20 +0100

    add logging function printing "Hello, world!"

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.debugging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logging()
     }
+}
+
+fun logging() {
+    Log.v(TAG, "Hello, world!")
 }
